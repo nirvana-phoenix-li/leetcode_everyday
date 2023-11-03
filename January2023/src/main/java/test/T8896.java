ed7e0d2631d2a6ea2283cc442f62a08682d79de3
ethan <2332054958@qq.com>
2023-11-03 22:54:48 +0800

    代码提交

--- January2023/src/main/java/test/T8896.java
+++ January2023/src/main/java/test/T8896.java
@@ -1,16 +1,19 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class T8896 {
 
     public static void main(String[] args) {
-        int help = 1;
-        while (true) {
-            if (help % 17 != 0) {
-                help = help * 10 + 1;
-            } else {
-                System.out.println(help);
-                break;
-            }
-        }
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>(7);
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>(8);
+        hashtable.put(2,2);
+        Integer uuuuu=8;
+        int i = uuuuu.hashCode();
+        System.out.println(i);
+        hashMap.put(1, 1);
+        System.out.println(hashMap);
+
     }
 }

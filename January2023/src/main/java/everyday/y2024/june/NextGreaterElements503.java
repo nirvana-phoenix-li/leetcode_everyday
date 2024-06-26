606dce46400d9b5fb9a694c888098a7712fd084e
li <li@lideMacBook-Pro.local>
2024-06-26 09:12:25 +0800

    code format

--- January2023/src/main/java/everyday/y2024/june/NextGreaterElements503.java
+++ January2023/src/main/java/everyday/y2024/june/NextGreaterElements503.java
@@ -22,25 +22,39 @@ public class NextGreaterElements503 {
             }
         }
 
-        if (arrayList.size()==0){
-            arrayList.add(nums[nums.length-1]);
-        }else {
+        if (arrayList.size() == 0) {
+            arrayList.add(nums[nums.length - 1]);
+        } else {
 
         }
 
 
         int[] answer = new int[nums.length];
 
+        int temp = Integer.MIN_VALUE;
+        int index=-1;
+
         for (int i = 0; i < arrayList.size(); i++) {
 
             int num = nums[arrayList.get(i)];
+            if (temp == Integer.MIN_VALUE) {
+                temp = num;
+                index=i;
+            }else {
+                if (temp<=num){
+
+                }else {
+
+                }
+            }
+
 
             for (int j = 0; j < arrayList.get(i); j++) {
-                answer[j]=answer[j+1];
+                answer[j] = answer[j + 1];
             }
+
+
         }
-
-
 
 
         return null;

f7611cf89c36843cda1da1f47e54abf29a6c94da
li <li@lideMacBook-Pro.local>
2024-07-22 09:23:42 +0800

    代码提交

--- src/main/java/test/T9999988.java
+++ src/main/java/test/T9999988.java
@@ -1,11 +1,27 @@
 package test;
 
-import java.time.LocalDate;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.TimeUnit;
 
 public class T9999988 {
     public static void main(String[] args) {
+        // 创建一个每秒允许150个许可的RateLimiter
+        long currentTimeMillis = System.currentTimeMillis();
+        RateLimiter rateLimiter = RateLimiter.create(20.0);
 
-        String s = LocalDate.now().toString();
-        System.out.println(s);
+        for (int i = 0; i < 1000; i++) {
+            // 从限流器中获取一个许可
+//            rateLimiter.tryAcquire(20, TimeUnit.MILLISECONDS);
+            rateLimiter.acquire();
+
+            // 执行某些操作
+            doSomething(i);
+        }
+        System.out.println("use time----------------" + (System.currentTimeMillis() - currentTimeMillis));
+    }
+
+    public static void doSomething(int i) {
+        System.out.println("Action " + i);
     }
 }

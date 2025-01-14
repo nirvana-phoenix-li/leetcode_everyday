14b8b318219df027766e801d62b8252522e89ea2
lisx <li@lideMacBook-Pro.local>
2025-01-14 15:35:30 +0800

    第一次提交

--- src/main/java/test/T4.java
+++ src/main/java/test/T4.java
@@ -1,18 +1,10 @@
 package test;
 
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUnit;
-import cn.hutool.core.date.DateUtil;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class T4 {
     public static void main(String[] args) throws ParseException {
-        String original = "{\"id\": 83,\"recordId\": 2982,\"memberId\": 215995502578101273,\"type\": \"expire_remind_more\"}";
-        String s = original.replaceAll(" ", "");
-        System.out.println(s);
 
 
     }
